dfdda01d65c7efa3fefef6dfc5f1ad3bc6376a48
Aljke <alkaris95@gmail.com>
2017-09-30 11:07:35 +0300

    Add EF to project

--- CashRegister/Startup.cs
+++ CashRegister/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using CashRegister.AppDbContext;
 
 namespace CashRegister
 {
@@ -27,8 +29,11 @@ namespace CashRegister
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
-            services.AddMvc();
+			// Add framework services.
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+			);
+			services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
