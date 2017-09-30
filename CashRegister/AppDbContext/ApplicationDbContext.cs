eaa86a3e32aee653240e6cf33b309d65c4d204e2
Aljke <alkaris95@gmail.com>
2017-09-30 11:08:14 +0300

    Add DB context and connection string

---
+++ CashRegister/AppDbContext/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CashRegister.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashRegister.AppDbContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+		   : base(options)
+		{
+		}
+
+		public DbSet<Product> Products { get; set; }
+		public DbSet<CheckProduct> CheckProducts { get; set; }
+		public DbSet<CheckData> Checks { get; set; }
+	}
+}
