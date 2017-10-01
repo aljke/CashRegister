3f2fdb14295e57a77fbcaceb5a06b20355c73569
Aljke <alkaris95@gmail.com>
2017-10-01 13:48:26 +0300

    Add vie models for check

---
+++ CashRegister/ViewModels/CheckViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CashRegister.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace CashRegister.ViewModels
+{
+	public class CheckViewModel
+    {
+		public DateTime DateTime { get; set; }
+		public List<ProductDataViewModel> Products { get; set; }
+		public decimal TotalPrice { get; set; }
+    }
+}
---
+++ CashRegister/ViewModels/ProductDataViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashRegister.ViewModels
+{
+    public class ProductDataViewModel
+    {
+		public string Caption { get; set; }
+		public decimal Price { get; set; }
+		public int Amount { get; set; }
+	}
+}
